dea7e28ddbc693ef822fca819bf000d62e1d631a
arhamathar <arhamathar789@gmail.com>
2021-06-18 17:08:51 +0530

    CORS added

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+	res.setHeader(
+		"Access-Control-Allow-Headers",
+		"Origin, X-Requested-With, Content-Type, Accept, Authorization"
+	);
+	next();
+});
+
 app.use("/api/user", userRoutes);
 
 connectDb()
