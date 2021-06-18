2791531e9a7be32445a17cfd5b252a9557e09814
arhamathar <arhamathar789@gmail.com>
2021-06-18 12:56:48 +0530

    user login route

--- controllers/user.js
+++ controllers/user.js
@@ -1,11 +1,19 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const { validationResult } = require("express-validator");
 const User = require("../models/users");
 
 const userSignup = async (req, res, next) => {
-	const { name, email, password } = req.body;
-	let hasUser;
+	const error = validationResult(req);
+	if (!error.isEmpty()) {
+		return res
+			.status(422)
+			.json("Invalid data entered, please enter data correctly");
+	}
 
+	const { name, email, password } = req.body;
+
+	let hasUser;
 	try {
 		hasUser = await User.findOne({ email });
 	} catch (error) {
@@ -59,10 +67,66 @@ const userSignup = async (req, res, next) => {
 	}
 
 	res.status(201).json({
+		message: "Sign Up Successful",
 		userId: newUser.id,
 		email: newUser.email,
 		token: token,
 	});
 };
 
+const userLogin = async (req, res, next) => {
+	const { email, password } = req.body;
+
+	let identifiedUser;
+	try {
+		identifiedUser = await User.findOne({ email });
+	} catch (error) {
+		return res.status(500).json("Logging In failed, please try again !");
+	}
+
+	if (!identifiedUser) {
+		return res.status(403).json("User not found , please check email.");
+	}
+
+	let isValidUser = false;
+	try {
+		isValidUser = await bcrypt.compare(password, identifiedUser.password);
+	} catch (err) {
+		console.log(err);
+		return res
+			.status(500)
+			.json(
+				"Could not log you in, please check your credentials and try again."
+			);
+	}
+
+	if (!isValidUser) {
+		return res
+			.status(401)
+			.json("Invalid credentials, please check your password.");
+	}
+
+	try {
+		token = jwt.sign(
+			{ userId: identifiedUser._id, email: identifiedUser.email },
+			process.env.JWT_SECRET_KEY,
+			{
+				expiresIn: "1h",
+			}
+		);
+	} catch (error) {
+		return res
+			.status(500)
+			.json({ error: "Logging user failed, please try again!" });
+	}
+
+	res.status(200).json({
+		message: "Login Successful",
+		userId: identifiedUser.id,
+		email: identifiedUser.email,
+		token: token,
+	});
+};
+
+exports.userLogin = userLogin;
 exports.userSignup = userSignup;
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { check } = require("express-validator");
 const userController = require("../controllers/user");
 
-router.post("/signup", userController.userSignup);
+router.post(
+	"/signup",
+	[
+		check("name").notEmpty(),
+		check("email").isEmail().normalizeEmail(),
+		check("password").isLength({ min: 6 }),
+	],
+	userController.userSignup
+);
+
+router.post("/login", userController.userLogin);
 
 module.exports = router;
